c47109775b76ce6a5d636959c4c402919da846b6
Jayques Nelson <jayquesnelson10@gmail.com>
2026-01-21 14:31:50 -0500

    Add files via upload

--- T-Test.R
+++ T-Test.R
@@ -10,3 +10,18 @@ pts = seq(-4.5,4.5,length=100)
 plot(pts,dt(pts,df=9),col='red',type='l')
 lines(density(x), col='green')
 lines(density(y), col='blue')
+
+#Apply the function for t-test
+ttest = t.test(x,y) 
+
+#T-test results
+Welch Two Sample t-test
+
+data:  x and y
+t = -1.1485, df = 15.59, p-value = 0.2681
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+  -1.0586475  0.3156739
+sample estimates:
+  mean of x   mean of y 
+-0.05163308  0.31985371 
