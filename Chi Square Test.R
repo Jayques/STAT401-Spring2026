d0c96dcb2e3c04c406bce4f59365d5295c0e1d68
Jayques Nelson <jayquesnelson10@gmail.com>
2026-04-15 14:20:05 -0400

    Add files via upload

---
+++ Chi Square Test.R
@@ -0,0 +1,18 @@
+#Jayques Nelson
+#April 15, 2026
+#Chi-Square Tets
+
+#Read dummy dataset
+data_frame <- read.csv("https://goo.gl/j6lRXD")
+
+#Print dataset in table form
+table(data_frame$treatment, data_frame$improvement)
+
+#Chi-Square function
+chisq.test(data_frame$treatment, data_frame$improvement, correct=FALSE)
+
+#Results
+Pearson's Chi-squared test
+
+data:  data_frame$treatment and data_frame$improvement
+X-squared = 5.5569, df = 1, p-value = 0.01841
